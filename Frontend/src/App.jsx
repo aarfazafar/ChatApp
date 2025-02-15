735ae7df5d85286c4939e426cd144c378250112c
Tanweer <tanweerjamal833@gmail.com>
2025-02-15 18:17:51 +0530

    Integration done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './Auth/Login'
 
 function App() {
 
   return (
-    <>
-      <div className='bg-green-300'>
-        <Login/>
-      </div>
-    </>
+    <div>
+      <Routes>
+        <Route path='/register' element={<Login/>}/>
+      </Routes>
+    </div>
   )
 }
 
