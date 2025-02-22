77b368193b489a62e3479df6316cf78af1162ae9
aarfazafar <aarfazafar2@gmail.com>
2025-02-23 00:43:07 +0530

    landing ui

--- Frontend/src/Components/LandingPage/LandingPage.jsx
+++ Frontend/src/Components/LandingPage/LandingPage.jsx
@@ -3,17 +3,17 @@ import "./LandingPage.css"
 import { Ghost } from 'lucide-react'
 const LandingPage = () => {
   return (
-    <div className='vtFont flex'>
-      <div className="left  w-[50%] h-[100vh]">
-        <h1 className='text-8xl font-bold pl-[5vw] mt-[32vh] text-[var(--color-accent)]'>Shadow Speaks Here</h1>
-        <p className='text-2xl pl-[5vw] text-[var(--color-accent)]'>&quot;Speak without a face. Vanish without a trace&quot;</p>
+    <div className='vtFont flex flex-col-reverse gap-4 lg:flex-row lg:gap-0 items-center justify-center min-h-screen px-6 md:px-12 lg:px-20 overflow-hidden'>
+      <div className="left w-full md:w-2/3 text-center md:text-left">
+        <h1 className='text-6xl lg:text-8xl font-bold text-[var(--color-accent)] md:ml-17 tracking-wider md:tracking-normal'>Shadow Speaks Here</h1>
+        <p className='text-xl md:text-2xl mt-4 text-[var(--color-accent)] md:ml-17'>&quot;Speak without a face. Vanish without a trace&quot;</p>
         <button onClick={(e) => {
           e.preventDefault();
           window.location.href = "/register"
-        }} className='testing w-[80%] h-[8vh]'>Join the Shadows</button>
+        }} className='testing w-full md:w-[80%] h-[50px] md:h-[8vh] md:mt-6 rounded-lg text-lg font-semibold tracking-wide'>Join the Shadows</button>
       </div>
-      <div className="right w-[50%] h-fit mt-[8vw] flex justify-center items-center landing-animation">
-        <Ghost className='h-[70vh] w-[70vh]' color='#4fff4f'/>
+      <div className="right w-full flex lg:block md:w-1/2 md:m-0 justify-center items-center landing-animation mt-10 md:mt-0">
+        <Ghost className='h-[32vh] md:h-[50vh] lg:h-[70vh] w-auto' color='#4fff4f'/>
       </div>
     </div>
   )
