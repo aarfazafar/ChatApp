e4f55b411ad96e2db6debe5ec85791b21e172ea3
Tanweer <tanweerjamal833@gmail.com>
2025-02-23 03:34:38 +0530

    Fixed bugs

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   base: '/',
   server: {
-    historyApiFallback: true
+    port: 3000, // Change if needed
   },
+  build: {
+    outDir: 'dist', // Default output directory
+  }
 })
